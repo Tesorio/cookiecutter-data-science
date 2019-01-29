482655ceb28c1ce23d202a770df19df850b112a4
Peter John Hadlaw <peter@tesorio.com>
2019-01-29 11:06:37 -0800

    Added missing extra line for flake8

--- {{ cookiecutter.module_name }}/src/blueprint.py
+++ {{ cookiecutter.module_name }}/src/blueprint.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 blueprint = Blueprint('{{ cookiecutter.module_name }}', __name__,
                       url_prefix='/{{ cookiecutter.module_name }}')
 
+
 @blueprint.route('/health')
 def health():
     return 'OK'
